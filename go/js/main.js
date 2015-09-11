315d22293cca8e6b7ba6a479da13a5d39ac611ec
timurkin <kazerxxx@yandex.ru>
2015-09-11 16:13:09 +0500

    121

--- go/js/main.js
+++ go/js/main.js
@@ -61,8 +61,8 @@ $(document).ready(function () {
             var s = document.querySelector(".s1");
             VK.Api.call('polls.getVoters', {
                 owner_id: '-67272468',
-                poll_id: 192517522,
-                answer_ids: 637091678,
+                poll_id: 194692746,
+                answer_ids: 644507579,
                 count: 1000
             }, function (Voters) {
                 if (Voters.response == undefined) {
