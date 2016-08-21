4406d1ee51b0024feacb80efa57f38fd954d512e
timurkin <kazerxxx@yandex.ru>
2016-08-21 20:46:30 +0600

    Update main.js

--- go/js/main.js
+++ go/js/main.js
@@ -61,8 +61,8 @@ $(document).ready(function () {
             var s = document.querySelector(".s1");
             VK.Api.call('polls.getVoters', {
                 owner_id: '-67272468',
-        poll_id: 230640428,
-        answer_ids: 768225729,
+        poll_id: 234358096,
+        answer_ids: 781186861,
         count: 1000
             }, function (Voters) {
                 if (Voters.response == undefined) {
